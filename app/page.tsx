28271f34af1947652058627ecdb9107c299735ad
Gaius-1 <agbofrederick230@gmail.com>
2024-02-13 17:20:28 +0000

    improve homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Mail } from "lucide-react"
+import { MonitorCheck } from "lucide-react"
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant="default" size="sm">
-      <Mail className="mr-2 h-4 w-4"/>Submit Mail
-    </Button>
+    <div className="flex h-screen items-center justify-center">
+      <Button variant="default" size="sm">
+        <MonitorCheck className="mr-2 h-4 w-4"/>Submit Mail
+      </Button>
+    </div>
   );
 }
